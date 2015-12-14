db0657dcbfd15366389b6a6d781983224132826f
Taavi Koosaar <kirjatuvi@msn.com>
2015-12-14 01:08:37 +0100

    #2 work item dialog opens, but the experience is not so good. Need two forms.

--- src/Vso.StateModelVisualization/scripts/app/StateModelVisualization.js
+++ src/Vso.StateModelVisualization/scripts/app/StateModelVisualization.js
@@ -35,93 +35,111 @@ define(["require", "exports", "VSS/Controls", "VSS/Controls/TreeView", "VSS/Cont
 
             // Get a WIT client to make REST calls to VSTS 
             var witClient = VssService.getCollectionClient(TfsWitRest.WorkItemTrackingHttpClient);
+
             var configuration = VSS.getConfiguration().action;
 
-            witClient.getWorkItemTypes(context.project.name).then(function(wits) {
-                witClient.getWorkItemTypeCategory(context.project.name).then(function (categories) {
-                    allWits = wits;
-                    allCategories = categories;
+            var afterGraphReady = function (callbackData) {
+                var data = graph.prepareVisualizationData(callbackData.name, allWits);
+                for (var i = 0; i < data.length; i++) {
+                    graph.addElements(data[i]);
+                }
 
-                    var hiddenCategory = categories.value.filter(function (category) { return category.referenceName === "Microsoft.HiddenCategory"; })[0];
+                //activate toolbar
+                mainMenu.EnableToolbar();
+                //hookup resize with cytoscape
+                $(window).on("resize", graph.resize);
+            }
 
-                    var treeContainer = $(".work-item-type-tree-container");
-                    var firstNode;
+            if (configuration) {
+                witClient.getWorkItem(configuration.workItemId, ["System.Id", "System.Title", "System.WorkItemType"]).then(function (wit) {
+                    $('.js-treeview').hide();
+                    var witTypeName = wit.fields["System.WorkItemType"];
+                    witClient.getWorkItemType(context.project.name, witTypeName).then(function (witType) {
+                        allWits = new Array();
+                        allWits.push(witType);
 
-                    function convertToTreeNodes(items) {
-                        var workItems = new TreeView.TreeNode("Work Items");
-                        workItems.expanded = true;
-                        var hiddenWorkItems = new TreeView.TreeNode("Hidden Work Items");
-                        hiddenWorkItems.expanded = true;
+                        graph.create(witType.name, afterGraphReady, { name: witType.name });
+                    });
+                });
+            } else {
+                witClient.getWorkItemTypes(context.project.name).then(function(wits) {
+                    witClient.getWorkItemTypeCategory(context.project.name).then(function(categories) {
+                        $('.js-treeview').show();
+                        allWits = wits;
+                        allCategories = categories;
 
-                        items.forEach(function(item) {
-                            var node = new TreeView.TreeNode(item.name);
-                            //default root
-                            var root = workItems;
+                        var hiddenCategory = categories.value.filter(function(category) { return category.referenceName === "Microsoft.HiddenCategory"; })[0];
 
-                            //double loop, not the best, but simplest. 8 items in hidden category.
-                            for (var i = 0; i < hiddenCategory.workItemTypes.length; i++) {
-                                if (hiddenCategory.workItemTypes[i].name === item.name) {
-                                    root = hiddenWorkItems;
-                                    break;
+                        var treeContainer = $(".work-item-type-tree-container");
+                        var firstNode;
+
+                        function convertToTreeNodes(items) {
+                            var workItems = new TreeView.TreeNode("Work Items");
+                            workItems.expanded = true;
+                            var hiddenWorkItems = new TreeView.TreeNode("Hidden Work Items");
+                            hiddenWorkItems.expanded = true;
+
+                            items.forEach(function(item) {
+                                var node = new TreeView.TreeNode(item.name);
+                                //default root
+                                var root = workItems;
+
+                                //double loop, not the best, but simplest. 8 items in hidden category.
+                                for (var i = 0; i < hiddenCategory.workItemTypes.length; i++) {
+                                    if (hiddenCategory.workItemTypes[i].name === item.name) {
+                                        root = hiddenWorkItems;
+                                        break;
+                                    }
                                 }
-                            }
 
-                            root.add(node);
-                            if (item.name === wits[0].name) {
-                                firstNode = node;
+                                root.add(node);
+                                if (item.name === wits[0].name) {
+                                    firstNode = node;
+                                }
+                            });
+
+                            var sortChildren = function(a, b) {
+                                if (a.text > b.text) {
+                                    return 1;
+                                }
+                                if (a.text < b.text) {
+                                    return -1;
+                                }
+                                // a must be equal to b
+                                return 0;
+                            };
+
+                            workItems.children.sort(sortChildren);
+                            hiddenWorkItems.children.sort(sortChildren);
+
+                            var result = new Array();
+                            result.push(workItems);
+                            result.push(hiddenWorkItems);
+                            return result;
+                        }
+
+                        var treeViewOptions = { nodes: convertToTreeNodes(wits) };
+                        treeView = Controls.create(TreeView.TreeView, treeContainer, treeViewOptions);
+
+
+
+                        // Attach selectionchanged event using a DOM element containing treeview
+                        treeContainer.bind("selectionchanged", function(e, args) {
+                            treeView.TreeNode = args.selectedNode;
+                            var selectedNode = args.selectedNode;
+                            if (selectedNode && selectedNode.text !== "Work Items" && selectedNode.text !== "Hidden Work Items") {
+                                graph.create(selectedNode.text, afterGraphReady, { name: selectedNode.text });
                             }
                         });
 
-                        var sortChildren = function(a, b) {
-                            if (a.text > b.text) {
-                                return 1;
-                            }
-                            if (a.text < b.text) {
-                                return -1;
-                            }
-                            // a must be equal to b
-                            return 0;
-                        };
+                        //show first WIT as default
+                        graph.create(wits[0].name, afterGraphReady, { name: wits[0].name });
 
-                        workItems.children.sort(sortChildren);
-                        hiddenWorkItems.children.sort(sortChildren);
-
-                        var result = new Array();
-                        result.push(workItems);
-                        result.push(hiddenWorkItems);
-                        return result;
-                    }
-
-                    var treeViewOptions = { nodes: convertToTreeNodes(wits) };
-                    treeView = Controls.create(TreeView.TreeView, treeContainer, treeViewOptions);
-
-                    var afterGraphReady = function(callbackData) {
-                        var data = graph.prepareVisualizationData(callbackData.name, allWits);
-                        for (var i = 0; i < data.length; i++) {
-                            graph.addElements(data[i]);
-                        }
-                    }
-
-                    // Attach selectionchanged event using a DOM element containing treeview
-                    treeContainer.bind("selectionchanged", function(e, args) {
-                        treeView.TreeNode = args.selectedNode;
-                        var selectedNode = args.selectedNode;
-                        if (selectedNode && selectedNode.text !== "Work Items" && selectedNode.text !== "Hidden Work Items") {
-                            graph.create(selectedNode.text, afterGraphReady, { name: selectedNode.text });
-                        }
+                        //select treenode
+                        treeView.TreeNode = firstNode;
                     });
-
-                    //show first WIT as default
-                    graph.create(wits[0].name, afterGraphReady, { name: wits[0].name });
-
-                    //select treenode
-                    treeView.TreeNode = firstNode;
-                    //activate toolbar
-                    mainMenu.EnableToolbar();
-                    //hookup resize with cytoscape
-                    $(window).on("resize", graph.resize);
                 });
-            });
+            }
         }
         return StateModelVisualization;
     })();
